d07171f4f3e1e53ac3206a3f68b52cbd58c10724
Aaron Trostle <aaron.trostle@gmail.com>
2013-11-03 23:44:06 -0800

    add basic deal listing

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 
 gem 'sinatra'
+gem 'httparty'
+gem 'json'
 
 group :development do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'httparty'
+require 'json'
+
+BASE_URI = "http://deals.expedia.com/beta/deals/hotels.json"
+
+def get_deals
+  retrieve_deals = HTTParty.get(BASE_URI)
+  @deals = JSON.parse(retrieve_deals.body)
+end
 
 get '/' do
   erb :index
+end
+
+get '/deals' do
+  get_deals
+  erb :deals
 end
